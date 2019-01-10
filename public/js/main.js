5bc968e5c2613281c08c1fdbc6ac538a17c564fb
Sean Mangosing <seanmangosing@gmail.com>
2019-01-10 06:22:23 -0800

    update menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
+var burger = document.querySelector('.burger');
+console.log(burger);
+
+var menu = document.querySelector('.navbar-menu')
+if (burger !== null) {
+ burger.addEventListener('click', function() {
+ burger.classList.toggle('is-active');
+ menu.classList.toggle('is-active');
+ });
+}
+
+
 window.onscroll = () => {
     const nav = document.querySelector('.navbar');
     if(this.scrollY <= 10) nav.style.backgroundColor = 'transparent'; else nav.style.backgroundColor = 'rgb(35, 39, 49)';
